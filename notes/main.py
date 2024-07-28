5cb72552cab2b336ae49f86989f88ffd010f0d54
Player14 <patrickagaba.pa@gmail.com>
2024-07-28 18:05:48 +0100

    Added method for returning JSON of note and book data Also added a small project plan

--- notes/main.py
+++ notes/main.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 import datetime
+import json
 
 # Book model class
 class book:
@@ -7,10 +8,23 @@ class book:
         super().__init__()
         self.name = name
         self.timestamp = timestamp
+    
+    # class to return JSON of note data
+    def printJSON(self):
+        construct = {"name": self.name, "timestamp": self.timestamp}
+        construct = json.dumps(construct)
+        return construct
 
+# note model class
 class note(book):
-    def __init__(self, title, timestamp, data):
-        super().__init__()
+    def __init__(self, book_name, book_timestamp, title, timestamp, data):
+        super().__init__(book_name, book_timestamp)
         self.title = title
         self.timestamp = timestamp
         self.data = data
+
+    # class to return JSON of note data
+    def printJSON(self):
+        construct = {"title": self.title, "timestamp": self.timestamp, "data": self.data}
+        construct = json.dumps(construct)
+        return construct
