0d26765865723be09bd5aaae75aaf7f5ea9b0ce5
Player14 <patrickagaba.pa@gmail.com>
2024-07-29 14:21:06 +0100

    Added todo class definitions, and also changed JSON output for easier parsing

--- notes/main.py
+++ notes/main.py
@@ -25,6 +25,22 @@ class note(book):
 
     # class to return JSON of note data
     def printJSON(self):
-        construct = {"title": self.title, "timestamp": self.timestamp, "data": self.data}
+        construct = {"note": {"title": self.title, "timestamp": self.timestamp, "data": self.data}}
+        construct = json.dumps(construct)
+        return construct
+
+# todo model class
+class todo(book):
+    def __init__(self, book_name, book_timestamp, title, timestamp, scheduled, deadline, data):
+        super().__init__(book_name, book_timestamp)
+        self.title = title
+        self.timestamp = timestamp
+        self.scheduled = scheduled
+        self.deadline = deadline
+        self.data = data
+
+    # class to return JSON of todo data
+    def printJSON(self):
+        construct = {"todo": {"title": self.title, "timestamp": self.timestamp, "scheduled": self.scheduled, "deadline": self.deadline, "data": self.data}}
         construct = json.dumps(construct)
         return construct
